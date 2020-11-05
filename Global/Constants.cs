cda682112f1043c5e9cc6469fd29484682c75d5e
Jacob Leung <fyrexian@gmail.com>
2020-11-05 03:37:11 -0800

    updated global from main2

--- Global/Constants.cs
+++ Global/Constants.cs
@@ -7,5 +7,6 @@ namespace Global
         public const string TESTING = "testing";
         public const string TESTING2 = "testing2";
         public const string TESTING3 = "testing3";
+        //adding from main2
     }
 }
