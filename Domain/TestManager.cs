f4739b48bb51f362dd7952de21b19ab9b06f8e35
Jacob Leung <fyrexian@gmail.com>
2020-11-05 03:08:02 -0800

    adding domain and global proj to subtree folder and adding solution to main repo

---
+++ Domain/TestManager.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Domain
+{
+    public class TestManager
+    {
+        public string test = "test";
+    }
+}
---
+++ Global/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Global
+{
+    public static class Constants
+    {
+        public const string TESTING = "testing";
+        public const string TESTING2 = "testing2";
+        public const string TESTING3 = "testing3";
+    }
+}
